dbc9de24ede9b082b0706f62cd3522d0da2167d8
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-14 08:53:11 +0000

    variables declaration

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,23 @@
+const accountId = 123451
+let accountEmail = "abc@google.com"
+var accountPassword = "1234"
+accountCity = "Gurgaon"
+let accountState= "0001"
+
+accountEmail = "asb@gmail.com"
+accountCity = "Noida"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+let i=5;
+if (i = 5) {
+    console.log(accountPassword=1111)
+}
+
+console.log(accountId);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
