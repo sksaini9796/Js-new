ff02bbfcb36bc8a71475c7a271b0397241f9fe3a
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-15 12:58:55 +0000

    Nums and Math

---
+++ .devcontainer/01_basics/nums_and_math.js
@@ -0,0 +1,31 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.49678;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ****************** Maths **********************
+
+// console.log(Math.PI);
+// console.log(Math.abs(-40));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(6.2));
+// console.log(Math.floor(8.6));
+
+console.log(Math.random()*100);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
