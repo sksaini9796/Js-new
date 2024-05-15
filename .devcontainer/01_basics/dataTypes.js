f88c90a019ffcb4772163d7f2228a28a1f35f083
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-15 05:44:43 +0000

    dataTypes

---
+++ .devcontainer/01_basics/dataTypes.js
@@ -0,0 +1,15 @@
+console.log("Saurabh");
+
+let name = "xyz";
+let age = 18;
+let isLoggedIn = False;
+
+// number => 2 to the power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique  
+
+// object
