755de0d95aebe820118c7597b6426dc199df9425
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-15 06:16:34 +0000

    conversionOperation

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = 10;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let isLoggedIn = 1;
+console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 30;
+let stringNumber = String (someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber); 
