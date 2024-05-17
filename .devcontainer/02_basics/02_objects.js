71d5aab4fe0c029b689ab48189e150525fbd1b1b
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-17 07:59:24 +0000

    02-objects

---
+++ .devcontainer/02_basics/02_objects.js
@@ -0,0 +1,37 @@
+
+const tinderuser = new Object()
+
+tinderuser.id = "123abc";
+tinderuser.name = "Sammy";
+tinderuser.isLoggedIn = false;
+
+// console.log(tinderuser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Saurabh",
+            lastname: "Saini"
+        } 
+    }
+}
+//  console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLoggedIn'));
