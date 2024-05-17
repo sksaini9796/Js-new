1b4d74bae22143c1e9ec06e52d0970bade8eb00c
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-17 10:27:37 +0000

    functions

--- .devcontainer/03_basics/functions.js
+++ .devcontainer/03_basics/functions.js
@@ -25,12 +25,41 @@ function addTwoNumbers(number1, number2){
 
 function loginUserMessage(userName){
     if(!userName){
-        console.log("Please enter username");
+        // console.log("Please enter username");
         return
     }
 
     return `${userName} Just Logged In`
 }
 // console.log(loginUserMessage("Saurabh"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,600,800));
+
+
+const user = {
+    username: "Saurabh",
+    id: 101
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and id is ${anyobject.id}`);
+
+}
+// handleObject(user)
+
+handleObject({
+    username: "abc",
+    id: 201
+})
+
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
