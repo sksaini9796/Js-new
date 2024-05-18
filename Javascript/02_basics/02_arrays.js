2c799ab44e6e8c4324fa147a0a47f6950906f4be
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-18 12:28:05 +0530

    02_Arrays

--- Javascript/02_basics/02_arrays.js
+++ Javascript/02_basics/02_arrays.js
@@ -2,7 +2,7 @@ const marvel_heros = ["Thor", "Ironman", "Spiderman"]
 
 const dc_heros = ["Superman", "Flash", "Batman"]
 
-// marvel_heros.push(dc_heros);
+// marvel_heros.push(dc_heros); 
 
 // console.log(marvel_heros);
 
