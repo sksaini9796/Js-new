e6c0a13651a9c3ba4ed5450e667528ac0362f6ee
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-18 08:55:41 +0000

    Truthy Values

---
+++ Javascript/04_control_flow/truthy.js
@@ -0,0 +1,14 @@
+const userEmail =""
+if(userEmail){
+    console.log(`Got user email`);
+}
+else {
+    console.log(`Don't have user email`);
+}
+
+// Falsy Values
+// false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+// Truthy Values 
+
+// "0", 'false', " ",  [], {}, function(){}
