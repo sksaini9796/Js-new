ec16e3950cf308e70882ce91e88e75ed764625cc
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-18 08:50:13 +0000

    If-else

---
+++ Javascript/04_control_flow/one.js
@@ -0,0 +1,12 @@
+
+
+const userEmail = []
+
+if (userEmail.length === 0) {
+    console.log(`Array is empty`);
+}
+
+const emptyObj = {}
+if (Object.keys(emptyObj === 0)) {
+    console.log(`Object is empty`);
+}
