9803765d4b80aa216d8b58030b62ee4fd33f3332
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-18 10:48:04 +0000

    Truthy

--- Javascript/04_control_flow/truthy.js
+++ Javascript/04_control_flow/truthy.js
@@ -12,3 +12,22 @@ else {
 // Truthy Values 
 
 // "0", 'false', " ",  [], {}, function(){}
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 20
+val1 =  null ?? 10 ?? 15
+console.log(val1);
+
+
+// Terniary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log(`less then 80`) : console.log(`more than 80`);
