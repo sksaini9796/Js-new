b46bbd9a9dcbef847b2dda66bf4b869cd7b2fcea
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-18 12:23:47 +0530

    nums_and_math

--- Javascript/01_basics/nums_and_math.js
+++ Javascript/01_basics/nums_and_math.js
@@ -5,7 +5,7 @@ const balance = new Number(100)
 console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2))
 
 // const otherNumber = 123.49678;
 // console.log(otherNumber.toPrecision(4));
