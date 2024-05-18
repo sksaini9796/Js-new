d540c893be6fb08e00caaeed11dfda1bcad42d5b
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-18 06:36:13 +0000

    iife

---
+++ .devcontainer/03_basics/iife.js
@@ -0,0 +1,13 @@
+// Immediately Involked Function Expression (IIFE)
+
+(function chai(){    // named iife
+    console.log("DB Connected");
+})();
+
+( () => { 
+    console.log(`DB connected two`);
+}) ();
+
+( (name) => {
+    console.log(`DB connected two ${name}`);
+}) ('Saurabh')
