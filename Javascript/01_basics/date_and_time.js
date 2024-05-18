9d2d4c6a9afb8b7fc95df6cb74596fe12ce30c24
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-05-18 12:26:06 +0530

    date_and_time

--- Javascript/01_basics/date_and_time.js
+++ Javascript/01_basics/date_and_time.js
@@ -1,6 +1,6 @@
 let myDate =  new Date();
 console.log(myDate.toString());              // Thu May 16 2024 06:35:27 GMT+0000 (Coordinated Universal Time)
-console.log(myDate.toDateString());          // Thu May 16 2024
+console.log(myDate.toDateString());          // Thu May 16 2024 
 console.log(myDate.toLocaleDateString());    // 5/16/2024
 console.log(myDate.toTimeString());          // 06:35:27 GMT+0000 (Coordinated Universal Time)
 console.log(myDate.getDate());               // 16
