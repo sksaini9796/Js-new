65471411ec210fed6619e747e74efc2eb882d899
sksaini9796 <shorabhkumarsaini@gmail.com>
2024-06-08 13:27:08 +0000

    object get set

---
+++ Javascript/10_classes_oops/object_get_set.js
@@ -0,0 +1,15 @@
+const User = {
+    _email: "abc@gmail.com",
+    password: "4321",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
